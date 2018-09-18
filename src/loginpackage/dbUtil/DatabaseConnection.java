65732a72bd6b69954d422151042a1f82ad4bc0af
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-09-18 09:57:23 +0300

    kkl

--- src/loginpackage/dbUtil/DatabaseConnection.java
+++ src/loginpackage/dbUtil/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     private static final String PASSWORD = "dbpassword";
     private static final String SQCONN   = "jdbc:sqlite:PatientList.sqlite";
 
-    private static final String MYSQLUSERNAME = "root";
+    private static final String MYSQLUSERNAME = "mysql.sys";
     private static final String MYSQLPASSWORD = "Hashem741";
     private static final String MYSQLCONN =  "jdbc:mysql://localhost:3306/mysql?autoReconnect=true&useSSL=false";
 
