2157786b359eb176c4d90b65f5079544e0672640
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-09-18 20:19:32 +0300

    rthrg

--- src/loginpackage/dbUtil/DatabaseConnection.java
+++ src/loginpackage/dbUtil/DatabaseConnection.java
@@ -15,6 +15,7 @@ public class DatabaseConnection {
 
     private static final String MYSQL_GOOGLE_USERNAME = "mysql.sys";
     private static final String MYSQL_GOOGLE_PASSWORD = "Hashem741";
+    private static final String MYSQL_GOOGLE_MINE = "jdbc:mysql://35.228.119.107/patientlist?useSSL=false";
     private static final String MYSQL_GOOGLE =  "jdbc:mysql://google/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false";
     private static final String MYSQL_GOOGLE_INSTANCE_CONN_NAME = "second-jet-216010:europe-north1:mhdaska";
     private static final String MYSQL_GOOGLE_PRIMARY_IP_ADDR = "35.228.119.107";
@@ -25,16 +26,26 @@ public class DatabaseConnection {
 
 
     public static Connection getConnection() throws SQLException {
-        String jdbcUrl = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s"
+        
+        String instanceConnectionName = "second-jet-216010:europe-north1:mhdaska";
+        String databaseName = "patientlist";
+        String username = "root";
+        String password = "Hashem741";
+        String jdbcUrl = String.format(
+                "jdbc:mysql://google/%s?cloudSqlInstance=%s"
                         + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
-                MYSQL_GOOGLE_DATABASE_NAME,MYSQL_GOOGLE_INSTANCE_CONN_NAME);
-        System.out.println(jdbcUrl);
-                try {
+                databaseName,
+                instanceConnectionName);
+
+
+
+        try {
                     //Class.forName("org.sqlite.JDBC");
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     //return DriverManager.getConnection(SQCONN);
                     //return DriverManager.getConnection(MYSQLCONN, MYSQLUSERNAME, MYSQLPASSWORD);
-                    return DriverManager.getConnection(jdbcUrl, MYSQL_GOOGLE_USERNAME, MYSQL_GOOGLE_PASSWORD);
+                    //return DriverManager.getConnection(MYSQL_GOOGLE_MINE, MYSQL_GOOGLE_USERNAME, MYSQL_GOOGLE_PASSWORD);
+                    return DriverManager.getConnection(jdbcUrl, username, password);
 
                 } catch (ClassNotFoundException ex) {
                     ex.printStackTrace();
