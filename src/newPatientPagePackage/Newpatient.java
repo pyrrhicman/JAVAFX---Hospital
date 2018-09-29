90d762d415f5f8917a8c21e04fec09b85aa8ba2a
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-09-29 04:33:24 +0300

    [p[\

--- src/newPatientPagePackage/Newpatient.java
+++ src/newPatientPagePackage/Newpatient.java
@@ -6,15 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.fxml.LoadException;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import sample.DatabaseClass;
@@ -25,13 +19,14 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.DuplicateFormatFlagsException;
 import java.util.ResourceBundle;
 //</editor-fold>
 
 public class Newpatient implements Initializable {
 
     //<editor-fold desc="FXML Elements">
+    @FXML
+    private AnchorPane anchorPane;
     @FXML
     private JFXButton enter;
     @FXML
@@ -78,7 +73,11 @@ public class Newpatient implements Initializable {
     private JFXColorPicker cp6;
     @FXML
     private JFXColorPicker cp7;
+    @FXML
+    private JFXColorPicker cp8;
 
+    @FXML
+    private JFXColorPicker cp9;
     //</editor-fold>
     private final static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÅabcdefghijklmnopqrstuvwxyz";
     private final static String NUMBERS = "0123456789";
@@ -103,32 +102,37 @@ public class Newpatient implements Initializable {
     private  static String JFX_ERROR_UNFOCUS_COLOR_1 = "#4B5D68";
     private  static String JFX_ERROR_UNFOCUS_COLOR_2 = "white";
 
-    private  static String FX_BUTTON_COLOR = "#FCFF31";
-    private  static String FX_BUTTON_SIDE_COLOR = "#FCFF31";
+    private  static String FX_BUTTON_COLOR = "#4B5D68";
+    private  static String FX_BUTTON_SIDE_COLOR = "#4B5D68";
 
-    String publicStyle = ""+
-            "-fx-base:" +  FX_BASE  + ";"+
-            "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
-            "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
-            "-fx-text-fill: "+FX_TEXT_FILL+";" +
-            "-fx-font-family: "+FX_FONT_FAMILY+";" +
-            "-fx-style:"+ FX_STYLE + ";" +
-            "-fx-font-size:"+FX_FONT_SIZE+";";
+    private  static String FX_PROMPT_TEXT_COLOR = "#4B5D68";
 
-    String publicERRORStyle = ""+
-            "-fx-base:" +  FX_BASE  + ";"+
-            "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_ERROR_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_ERROR_UNFOCUS_COLOR_2+   " 75%);" +
-            "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
-            "-fx-text-fill: "+FX_TEXT_FILL+";" +
-            "-fx-font-family: "+FX_FONT_FAMILY+";" +
-            "-fx-style:"+ FX_STYLE + ";" +
-            "-fx-font-size:"+FX_FONT_SIZE+";";
+    private  static String FX_ANCHOR_BACKGROUND_COLOR = "#4B5D68";
 
+    //<editor-fold desc="TEXTFIELD STYLE">
+    private String textField_Style_Font;
+    private String textField_Style;
+    private String textField_Style_err;
 
+    private String comboBox_Style_Font;
+    private String comboBox_Style;
+    private String comboBox_Style_err;
+
+    private String datePicker_Style_Font;
+    private String datePicker_Style;
+    private String datePicker_Style_err;
+
+    private String button_Style_Font;
+    private String button_Style;
+    private String button_Style_err;
+
+    //<editor-fold desc="ANCHOR PANE STYLE">
+    private String anchorPane_Style = "-fx-background-color: " + FX_ANCHOR_BACKGROUND_COLOR + ";";
+    //private String anchorPane_Style_err = "-fx-background-color: " + FX_ANCHOR_BACKGROUND_COLOR + ";";
+    //</editor-fold>
 
 
     class TextfieldlistenerEditor {
-
 
 
         TextfieldlistenerEditor(JFXTextField jfxTextField, String string) {
@@ -140,15 +144,16 @@ public class Newpatient implements Initializable {
                 }
                 else
                 {
+
                     System.out.println(jfxTextField.getId()+"  out focus");
 
                     if (isThisFieldCorrect(jfxTextField.getText(),string)) {
                         System.out.println(jfxTextField.getId()+"  is formatted");
-                        jfxTextField.setStyle(publicStyle);
+                        jfxTextField.setStyle(textField_Style);
 
                     } else {
                         System.out.println(jfxTextField.getId()+"  is not formatted");
-                        jfxTextField.setStyle(publicERRORStyle);
+                        jfxTextField.setStyle(textField_Style_err);
                     }
 
 
@@ -218,6 +223,49 @@ public class Newpatient implements Initializable {
         };
         enterbirthday.setOnAction(birthdayAvailable);
 
+        EventHandler<MouseEvent> mouseEvent = event -> {
+
+            System.out.println(event.toString());//36-42
+            String string = event.toString().substring(36, 42);
+            switch (string) {
+                case "id=cp1":
+                    systext.setText("FX_FOCUS_COLOR");
+                    break;
+                case "id=cp2":
+                    systext.setText("JFX_UNFOCUS_COLOR_1");
+                    break;
+                case "id=cp3":
+                    systext.setText("JFX_UNFOCUS_COLOR_2");
+                    break;
+                case "id=cp4":
+                    systext.setText("FX_BASE");
+                    break;
+                case "id=cp5":
+                    systext.setText("FX_TEXT_FILL");
+                    break;
+                case "id=cp6":
+                    systext.setText("FX_BUTTON_COLOR");
+                    break;
+                case "id=cp7":
+                    systext.setText("FX_BUTTON_SIDE_COLOR");
+                    break;
+                case "id=cp8":
+                    systext.setText("FX_PROMPT_TEXT_COLOR");
+                    break;
+                case "id=cp9":
+                    systext.setText("FX_ANCHOR_BACKGROUND_COLOR");
+                    break;
+            }
+        };
+        cp1.setOnMouseEntered(mouseEvent);
+        cp2.setOnMouseEntered(mouseEvent);
+        cp3.setOnMouseEntered(mouseEvent);
+        cp4.setOnMouseEntered(mouseEvent);
+        cp5.setOnMouseEntered(mouseEvent);
+        cp6.setOnMouseEntered(mouseEvent);
+        cp7.setOnMouseEntered(mouseEvent);
+        cp8.setOnMouseEntered(mouseEvent);
+        cp9.setOnMouseEntered(mouseEvent);
         TextfieldlistenerEditor firstname_Listener  = new TextfieldlistenerEditor(firstname,VALID_NAME_FORMAT);
         TextfieldlistenerEditor lastname_Listener  = new TextfieldlistenerEditor(lastname,VALID_NAME_FORMAT);
         TextfieldlistenerEditor socialid_Listener  = new TextfieldlistenerEditor(socialid,VALID_SOCIAL_ID_FORMAT);
@@ -231,31 +279,48 @@ public class Newpatient implements Initializable {
         cp3.setValue(Color.valueOf(JFX_UNFOCUS_COLOR_2));
         cp4.setValue(Color.valueOf(FX_BASE));
         cp5.setValue(Color.valueOf(FX_TEXT_FILL));
-        /*cp6.setValue(Color.valueOf(FX_BUTTON_COLOR));
-        cp7.setValue(Color.valueOf(FX_BUTTON_SIDE_COLOR));*/
-    }
+        cp6.setValue(Color.valueOf(FX_BUTTON_COLOR));
+        cp7.setValue(Color.valueOf(FX_BUTTON_SIDE_COLOR));
+        cp8.setValue(Color.valueOf(FX_PROMPT_TEXT_COLOR));
+        cp9.setValue(Color.valueOf(FX_ANCHOR_BACKGROUND_COLOR));
 
+    }
 
     public void customstylesetter() {
 
         //<editor-fold desc="setSTYLE">
         styleupdate();
-        firstname.setStyle(publicStyle);
-        lastname.setStyle(publicStyle);
-        socialid.setStyle(publicStyle);
-        gender.setStyle(publicStyle);
-        age.setStyle(publicStyle);
-        registerationday.setStyle(publicStyle);
-        birthday.setStyle(publicStyle);
-        //birthday.setDefaultColor(Color.valueOf("#62929a"));
-        phonenumber.setStyle(publicStyle);
-        city.setStyle(publicStyle);
-        address.setStyle(publicStyle);
-        postalcode.setStyle(publicStyle);
-        systext.setText(publicStyle);
-        enter.setStyle(publicStyle);
-        cancel.setStyle(publicStyle);
-        settheme.setStyle(publicStyle);
+
+        //ANCHOR PANE
+        anchorPane.setStyle(anchorPane_Style);
+
+        //TEXT FIELDS
+        firstname.setStyle(textField_Style_Font + textField_Style);
+        lastname.setStyle(textField_Style_Font + textField_Style);
+        socialid.setStyle(textField_Style_Font + textField_Style);
+        age.setStyle(textField_Style_Font + textField_Style);
+        phonenumber.setStyle(textField_Style_Font + textField_Style);
+        city.setStyle(textField_Style_Font + textField_Style);
+        address.setStyle(textField_Style_Font + textField_Style);
+        postalcode.setStyle(textField_Style_Font + textField_Style);
+
+        //COMBO BOX
+        gender.setStyle(comboBox_Style_Font+comboBox_Style);
+
+        //DATE PICKER
+        registerationday.setStyle(datePicker_Style_Font+datePicker_Style);
+        birthday.setStyle(datePicker_Style_Font+datePicker_Style);
+
+        //BUTTONS
+        enter.setStyle(button_Style_Font + button_Style +"-fx-background-color: "+ FX_BUTTON_COLOR+";");
+        cancel.setStyle(button_Style_Font + button_Style +"-fx-background-color: "+ FX_BUTTON_COLOR+";");
+        settheme.setStyle(button_Style_Font + button_Style +"-fx-background-color: "+ FX_BUTTON_COLOR+";");
+
+        //TEXT
+        systext.setText(textField_Style);
+
+
+
         //</editor-fold>
         //enter.setStyle("-fx-background-color: "+ FX_BUTTON_COLOR+";" + "-fx-border-color:transparent transparent transparent "+FX_BUTTON_SIDE_COLOR +";");
         //cancel.setStyle("-fx-background-color: "+ FX_BUTTON_COLOR+";");
@@ -283,14 +348,22 @@ public class Newpatient implements Initializable {
         FX_TEXT_FILL = "#" + Integer.toHexString(cp5.getValue().hashCode());
         FX_TEXT_FILL = FX_TEXT_FILL.substring(0, 7);
 
-        /*FX_BUTTON_COLOR = "#" + Integer.toHexString(cp6.getValue().hashCode());
+        FX_BUTTON_COLOR = "#" + Integer.toHexString(cp6.getValue().hashCode());
         FX_BUTTON_COLOR = FX_BUTTON_COLOR.substring(0, 7);
 
         FX_BUTTON_SIDE_COLOR = "#" + Integer.toHexString(cp7.getValue().hashCode());
-        FX_BUTTON_SIDE_COLOR = FX_BUTTON_SIDE_COLOR.substring(0, 7);*/
+        FX_BUTTON_SIDE_COLOR = FX_BUTTON_SIDE_COLOR.substring(0, 7);
+
+        FX_PROMPT_TEXT_COLOR = "#" + Integer.toHexString(cp8.getValue().hashCode());
+        FX_PROMPT_TEXT_COLOR = FX_PROMPT_TEXT_COLOR.substring(0, 7);
+
+        FX_ANCHOR_BACKGROUND_COLOR = "#" + Integer.toHexString(cp9.getValue().hashCode());
+        FX_ANCHOR_BACKGROUND_COLOR = FX_ANCHOR_BACKGROUND_COLOR.substring(0, 7);
+
+
         customstylesetter();
         System.out.println("theme is set " + "FX_FOCUS_COLOR: " + FX_FOCUS_COLOR + " JFX_UNFOCUS_COLOR_1: " + JFX_UNFOCUS_COLOR_1+ " JFX_UNFOCUS_COLOR_2: " + JFX_UNFOCUS_COLOR_2 + " FX_BASE: " + FX_BASE );
-        System.out.println(publicStyle);
+        System.out.println(textField_Style);
 
 
     }
@@ -405,16 +478,21 @@ public class Newpatient implements Initializable {
     }
 
     private void styleupdate() {
-        String publicStyle = ""+
-                "-fx-base:" +  FX_BASE  + ";"+
-                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
-                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
-                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+        //<editor-fold desc="TEXTFIELD STYLE">
+         String textField_Style_Font = ""+
                 "-fx-font-family: "+FX_FONT_FAMILY+";" +
                 "-fx-style:"+ FX_STYLE + ";" +
                 "-fx-font-size:"+FX_FONT_SIZE+";";
 
-        String publicERRORStyle = ""+
+         String textField_Style = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-border-color: transparent transparent transparent transparent"+";" +
+                "-fx-prompt-text-fill: "+ FX_PROMPT_TEXT_COLOR + ";";
+
+         String textField_Style_err = ""+
                 "-fx-base:" +  FX_BASE  + ";"+
                 "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_ERROR_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_ERROR_UNFOCUS_COLOR_2+   " 75%);" +
                 "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
@@ -422,11 +500,112 @@ public class Newpatient implements Initializable {
                 "-fx-font-family: "+FX_FONT_FAMILY+";" +
                 "-fx-style:"+ FX_STYLE + ";" +
                 "-fx-font-size:"+FX_FONT_SIZE+";";
+        //</editor-fold>
 
-        this.publicStyle = publicStyle;
-        this.publicERRORStyle = publicERRORStyle;
+        //<editor-fold desc="COMBOBOX STYLE">
+         String comboBox_Style_Font = ""+
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+
+
+         String comboBox_Style = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-border-color: transparent transparent transparent transparent"+";" +
+                "-fx-prompt-text-fill: "+ FX_PROMPT_TEXT_COLOR + ";";
+
+         String comboBox_Style_err = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_ERROR_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_ERROR_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+        //</editor-fold>
+
+        //<editor-fold desc="DATEPICKER STYLE">
+         String datePicker_Style_Font = ""+
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+         String datePicker_Style = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";" +
+                "-fx-border-color: transparent transparent transparent transparent"+";" +
+                "-fx-prompt-text-fill: "+ FX_PROMPT_TEXT_COLOR + ";";
+
+         String datePicker_Style_err = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_ERROR_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_ERROR_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+        //</editor-fold>
+
+        //<editor-fold desc="BUTTON STYLE">
+         String button_Style_Font = ""+
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+
+         String button_Style = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-border-color: transparent transparent transparent "+FX_BUTTON_SIDE_COLOR +";" +
+                "-fx-prompt-text-fill: "+ FX_PROMPT_TEXT_COLOR + ";";
+
+         String button_Style_err = ""+
+                "-fx-base:" +  FX_BASE  + ";"+
+                "-jfx-unfocus-color: linear-gradient(to left, "  +JFX_ERROR_UNFOCUS_COLOR_1+ " 10%" +   ","   +JFX_ERROR_UNFOCUS_COLOR_2+   " 75%);" +
+                "-jfx-focus-color: " + FX_FOCUS_COLOR +";"+
+                "-fx-text-fill: "+FX_TEXT_FILL+";" +
+                "-fx-font-family: "+FX_FONT_FAMILY+";" +
+                "-fx-style:"+ FX_STYLE + ";" +
+                "-fx-font-size:"+FX_FONT_SIZE+";";
+        //</editor-fold>
+
+        //<editor-fold desc="ANCHOR PANE STYLE">
+         String anchorPane_Style = "-fx-background-color: " + FX_ANCHOR_BACKGROUND_COLOR + ";";
+        //private String anchorPane_Style_err = "-fx-background-color: " + FX_ANCHOR_BACKGROUND_COLOR + ";";
+        //</editor-fold>
+
+
+        this.textField_Style_Font= textField_Style_Font;
+        this.textField_Style_err=textField_Style_err;
+        this.textField_Style = textField_Style;
+
+        this.comboBox_Style = comboBox_Style;
+        this.comboBox_Style_err = comboBox_Style_err;
+        this.comboBox_Style_Font = comboBox_Style_Font;
+
+        this.datePicker_Style = datePicker_Style;
+        this.datePicker_Style_err = datePicker_Style_err;
+        this.datePicker_Style_Font = datePicker_Style_Font;
+
+        this.button_Style = button_Style;
+        this.button_Style_err = button_Style_err;
+        this.button_Style_Font = button_Style_Font;
+
+        this.anchorPane_Style = anchorPane_Style;
+
+
 
     }
+
+
 
 }
 
